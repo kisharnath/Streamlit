05778e7add18ad6cc1db77597174651b4c770a45
Kishar Kr Nath <85046952+kisharnath@users.noreply.github.com>
2024-04-17 20:33:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,18 @@
 import streamlit as st
-st.text_input("Your name", key="name")
-st.text_input("Second number", key="second")
-st.text_input("Third number", key="third")
+st.title("Compare three numbers")
+number1 = st.number_input("Insert a number", value=None, placeholder="Type a number...")
+number2 = st.number_input("Insert a number", value=None, placeholder="Type a number...")
+number3 = st.number_input("Insert a number", value=None, placeholder="Type a number...")
+button = st.button("Compare")
+if button:
+  if number1 > number2 :
+    if number3 > number1 :
+      st.write("Largest number",number3)
+    else:
+      st.write("Larges number ", numebr1)
+  else:
+    if number2 > number3:
+      st.write("Largest number ", number2)
+    else:
+      st.write("Largest number", number3)
+    
